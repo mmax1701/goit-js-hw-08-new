f7c2847a42904a68b9744c6abc8c7e2afd9f33ba
mmax1701 <122221954+mmax1701@users.noreply.github.com>
2024-09-23 20:59:30 +0300

    task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,29 @@
+import Player from '@vimeo/player';
 
+const iframe = document.querySelector('#vimeo-player')
+const player = new Player(iframe);
+
+
+function onPlay(data) {
+    localStorage.setItem("videoplayer-current-time", JSON.stringify(data))
+}
+
+player.on('timeupdate', onPlay);
+
+const getTime = JSON.parse(localStorage.getItem("videoplayer-current-time")) ?? '0';
+const time = getTime.seconds;
+
+
+player.setCurrentTime(time).then(function(seconds) {
+    // seconds = the actual time that the player seeked to
+}).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+            // the time was less than 0 or greater than the video’s duration
+            break;
+
+        default:
+            // some other error occurred
+            break;
+    }
+});
