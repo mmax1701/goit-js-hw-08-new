a88e0ccd3d24d5801493a5e8d810f713271a8f37
mmax1701 <122221954+mmax1701@users.noreply.github.com>
2024-09-23 19:18:46 +0300

    1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,21 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
-// Change code below this line
+// Описан в документации
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 
-console.log(galleryItems);
+const list = document.querySelector('.gallery')
+
+galleryItems.map(item => {
+    list.insertAdjacentHTML('beforeend', `<li class="gallery__item">
+   <a class="gallery__link" href="${item.original}">
+      <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+   </a>
+</li>`)
+});
+
+
+new SimpleLightbox('.gallery__link', {
+    captionsData: 'alt',
+    captionDelay: 250
+ });
