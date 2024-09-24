069be245645609198d60e5a4d62d5fa962d65b26
mmax1701 <122221954+mmax1701@users.noreply.github.com>
2024-09-24 21:47:56 +0300

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,34 @@
+import throttle from "lodash.throttle";
 
+const KEY = "feedback-form-state";
+const form = document.querySelector('.feedback-form')
+
+const throtForm = throttle(onInput, 500)
+
+form.addEventListener('input', throtForm)
+form.addEventListener('submit', onSubmit)
+
+const data = JSON.parse(localStorage.getItem(KEY)) || {};
+
+form.elements.email.value = data.email ?? "";
+form.elements.message.value = data.message ?? "";
+
+
+function onInput(evt) {
+    const formVal = {
+        email: evt.currentTarget.email.value,
+        message: evt.currentTarget.message.value
+    }
+
+    localStorage.setItem(KEY, JSON.stringify(formVal))
+}
+
+
+
+function onSubmit(evt) {
+    evt.preventDefault();
+    console.log(`email: ${evt.currentTarget.email.value}`);
+    console.log(`message: ${evt.currentTarget.message.value}`);
+    localStorage.clear()
+    form.reset();
+}
